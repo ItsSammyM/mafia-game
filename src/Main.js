7657471f2db5356cfdb07dfb54f6a94147a69c92
Sam Maselli <sammypet3@gmail.com>
2023-01-17 09:12:27 -0500

    iphone test sound

--- src/Main.js
+++ src/Main.js
@@ -9,11 +9,22 @@ export class Main extends React.Component {
         Main.instance = this;
 
         this.state = {
+            soundEffect: new Audio(),
             currentMenu: <StartMenu />,
             audioElement: null,
         };
     }
     componentDidMount() {
+        //const soundEffect = new Audio();
+        this.state.soundEffect.autoplay = true;
+
+        // onClick of first interaction on page before I need the sounds
+        // (This is a tiny MP3 file that is silent and extremely short - retrieved from https://bigsoundbank.com and then modified)
+        this.state.soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+        this.setState({soundEffect: this.state.soundEffect});
+        // later on when you actually want to play a sound at any point without user interaction
+        //this.state.soundEffect.src = 'vine_boom.mp3';
+        //this.state.soundEffect.play();
     }
     componentWillUnmount() {
     }
@@ -36,19 +47,22 @@ export class Main extends React.Component {
     //   asyncCall();
 
     playSound(){
-        this.setState({audioElement: 
-            <audio autoPlay>
-            {/* <source src="vine_boom.ogg" type="audio/ogg"></source> */}
-            <source src="vine_boom.mp3" type="audio/mpeg"></source>
-            Your browser does not support audio.
-            </audio>
-        });
+        // this.setState({audioElement: 
+        //     <audio autoPlay>
+        //     {/* <source src="vine_boom.ogg" type="audio/ogg"></source> */}
+        //     <source src="vine_boom.mp3" type="audio/mpeg"></source>
+        //     Your browser does not support audio.
+        //     </audio>
+        // });
         
-        new Promise(() => {
-            setTimeout(() => {
-                this.setState({audioElement:null});
-            }, 3000);
-        });
+        // new Promise(() => {
+        //     setTimeout(() => {
+        //         this.setState({audioElement:null});
+        //     }, 3000);
+        // });
+
+        this.state.soundEffect.src = 'vine_boom.mp3';
+        this.state.soundEffect.play();
     }
     render() {return (<div className="body">
         <br/>
--- src/menuComponents/Button.js
+++ src/menuComponents/Button.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Main } from "../Main";
 import "../styles/Main.css"
 
 /**
@@ -16,6 +17,11 @@ export function Button(props){
             style={{"backgroundColor": props.color, "width":props.width?props.width:"90%"}}
             onClick={
                 ()=>{
+                    //test for iphone sound effects.
+                    Main.instance.state.soundEffect.src = "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
+                    Main.instance.setState({soundEffect: Main.instance.state.soundEffect});
+                    Main.instance.state.soundEffect.play();
+
                     (props.onClick ? props.onClick : ()=>{})()
                 }
             }>
